d6453e57ec9c9dc4ae3739f63c903e7dc4061ca4
grace.fidele <gngrace10@gmail.com>
2023-01-19 20:45:26 +0200

    basic structure of dashboard page

--- views/dashboard.php
+++ views/dashboard.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <title>Dashboard</title>
 </head>
 <body>
-    
+   <div class="container">
+    <div class="navbar">
+        <i class="fa fa-download"></i>
+    </div>
+    <div class="sidebar"></div>
+    <div class="main"></div>
+   </div> 
 </body>
 </html>
